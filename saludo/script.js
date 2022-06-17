b8842e0a43a5d45e63eb8dd05aa37413db7dddf0
Jesus Aguirre <aguirreruiz.jesus.daniel@gmail.com>
2022-06-17 15:17:58 -0500

    se utilizo innerText

--- saludo/script.js
+++ saludo/script.js
@@ -2,20 +2,18 @@ console.log(`Se nota que todavia me extranas`);
 
 
 setInterval(cambiarSaludo, 2000);
-//let frase = document.getElementsByTagName("h2")[1].innerHTML;
-function cambiarSaludo(){
-    //console.log(`me parece una falta de respeto`);
-    let frase = document.getElementsByTagName("h2")[1];
-    //console.log(`frase ${frase}`);
 
+/***
+ * Cambia la frase en el titulo h2
+ */
+
+function cambiarSaludo(){
+    let frase = document.getElementsByTagName("h2")[1];
     if(frase.innerHTML == "Ganando como siempre"){
-    //if(document.getElementsByTagName("h2")[1].innerHTML == "Ganando como siempre"){
-        frase.innerHTML = "impongo moda para las ni;as";
-        //document.getElementsByTagName("h2")[1].innerHTML= "impongo moda para las ni;as";
+        frase.innerHTML = "Impongo moda para las ni;as!";
     }
     else{
         frase.innerHTML = "Ganando como siempre";
-        //document.getElementsByTagName("h2")[1].innerHTML= "Ganando como siempre";
     }
     //console.log(`frase ${frase}`);
 }
